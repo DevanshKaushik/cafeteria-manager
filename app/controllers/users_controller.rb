1fdc48b14426017facbafc542ff18bb08faa1bbf
DevanshKaushik <devanshkaushik0711@gmail.com>
2021-01-27 22:11:16 +0530

    Added Users Model and Controller for creating a new user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def new
+  end
+
+  def create
+    role = params[:role].nil? ? "customer" : params[:role]
+
+    new_user = User.create!(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password_digest: params[:password],
+      role: role,
+    )
+    response_text = "New user is created with the user_id: #{new_user.id}"
+    render plain: response_text
+  end
+end
