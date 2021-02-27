a971860a1f95e56599cebb38ce6dd222eda033e3
DevanshKaushik <devanshkaushik0711@gmail.com>
2021-02-27 18:19:04 +0530

    Added ability to view all the users and update user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,51 @@
 class UsersController < ApplicationController
   skip_before_action :ensure_user_login
 
+  before_action(only: [:index, :show, :destroy, :edit]) { check_privilige("owner") }
+
+  def index
+  end
+
+  def show
+    @id = params[:id]
+  end
+
+  def edit
+    @id = params[:id]
+    render "edit"
+  end
+
+  def destroy
+    id = params[:id]
+    user = User.find_by(id: id)
+
+    if user
+      user.orders.each { |order| order.order_items.each { |item| item.destroy } }
+      user.orders.each { |order| order.destroy }
+      user.destroy
+    else
+      flash[:error] = "No user found"
+    end
+
+    redirect_to "/"
+  end
+
+  def update
+    role = params[:role].nil? ? "customer" : params[:role]
+
+    user = User.find(params[:id])
+    user.first_name = params[:first_name]
+    user.last_name = params[:last_name]
+    user.email = params[:email]
+    user.role = role
+
+    if !user.save
+      flash[:error] = user.errors.full_messages.join("\n")
+    end
+
+    redirect_to "/"
+  end
+
   def new
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :orders
+
+  def self.clerks
+    all.where(role: "clerk")
+  end
+
+  def self.not_owner
+    all.where.not(role: "owner")
+  end
 end
